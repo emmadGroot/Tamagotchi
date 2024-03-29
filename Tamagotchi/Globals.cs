15beae494e479980f14c02bfc8fd6cb781bd69bf
emmadGroot <emma@de-groot.it>
2024-03-29 15:59:19 +0100

    fixed typo

--- Tamagotchi/Globals.cs
+++ Tamagotchi/Globals.cs
@@ -8,7 +8,7 @@
             new CommandData("stats", Commands.ShowStats, "Shows your Tamagotchi's stats"),
             new CommandData("feed", Commands.Feed, "Feed your Tamagotchi - add parameter to feed more or less"),
             new CommandData("play", Commands.Play, "Play with your Tamagotchi - add parameter to play more or less"),
-            new CommandData("rename", Commands.Rename, "Rename your Tamagotchi - add paramter to skip prompt"),
+            new CommandData("rename", Commands.Rename, "Rename your Tamagotchi - add parameter to skip prompt"),
             new CommandData("clear", Commands.Clear, "Clear the console"),
             new CommandData("kill", Commands.Kill, "Almost kill your Tamagothi (for debug purposes)"),
             new CommandData("exit", Commands.Exit, "Exit the application")
